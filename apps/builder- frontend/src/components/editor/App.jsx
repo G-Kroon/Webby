8b8bd82c3b74d0b2c4f07b773276314ae65174d1
Jonathan Peters <jonathanpeters051@gmail.com>
2025-11-21 14:11:36 +0200

    Create App.jsx
    Signed-off-by: Jonathan Peters <jonathanpeters051@gmail.com>

---
+++ apps/builder- frontend/src/components/editor/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import EditorShell from "./EditorShell";
+
+export default function App() {
+  return (
+    <div className="app">
+      <EditorShell />
+    </div>
+  );
+}
